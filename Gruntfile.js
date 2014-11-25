0274cf6f333a73070001267ef44fa7ea0019e6fa
Matthew Daly <matthewbdaly@gmail.com>
2014-11-25 18:53:28 +0000

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'test/*.js',
+                'index.js'
+            ]
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: 'test', // the folder, not the files,
+                options: {
+                    mask: '*.js',
+                    reportFormats: ['cobertura', 'html', 'lcovonly']
+                }
+            }
+        },
+        coveralls: {
+            options: {
+                src: 'coverage/lcov.info',
+                force: false
+            },
+            app: {
+                src: 'coverage/lcov.info'
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+    // Register tasks
+    grunt.registerTask('test', ['jshint', 'mocha_istanbul:coverage', 'coveralls']);
+};
