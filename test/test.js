5044a72b4e487254cbf05d88c4a7457cb5136c7d
Matthew Daly <matthewbdaly@gmail.com>
2014-11-25 20:38:58 +0000

    Fixed a glitch in the test

--- test/test.js
+++ test/test.js
@@ -29,7 +29,7 @@ describe('server', function () {
     // Test the index route
     describe('Test the index route', function () {
         it('should return a page with the title Babblr', function (done) {
-            request.get({ url: 'http://localhost:5000' }, function (error, response, body) {
+            request.get({ url: 'http://localhost:5000/' }, function (error, response, body) {
                 expect(body).to.include('Babblr');
                 expect(response.statusCode).to.equal(200);
                 expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
